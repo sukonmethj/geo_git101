dae0c4118f8e06fa2c2c91fc7136b7268e489fe5
Sukonmeth <sukonmethj@gmail.com>
2025-07-01 11:22:21 +0700

    Add save figure

--- eq_test.py
+++ eq_test.py
@@ -1,4 +1,15 @@
 from obspy import read
 
 st = read()
-st.plot()
+# st.plot()
+st[1].spectrogram(log=True)
+
+# Filter the waveform
+tr = st[1].copy()
+tr.filter('bandpass', freqmin=1.0, freqmax=5.0, corners=4, zerophase=True)
+tr.plot()
+
+tr.write('demo_eq.mseed', format='MSEED')
+
+fig=tr.plot(show=False)
+fig.savefig('demo_waveform.png', dpi=300)
